50d36208db871f3275a0b9aab0878bacd9b7a8c4
eliandrade26 <eli_carlos_14@hotmail.com>
2024-03-28 22:17:49 -0300

    ALteração

--- script.js
+++ script.js
@@ -23,22 +23,22 @@ const playMachine = () => {
 }
 
 const playTheGame = (human, machine) => {
-    console.log('Humano :' + human + 'Maquina:' + machine)
+    console.log('Humano :' + human + 'Maquina:' + machine);
 
 
-    if (human === machine) {
-        result.innerHTML = "Deu empate"
-    } else if (
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === ' scissors' && machine === ' paper')
-    ) {
-        humanScoreNumber ++
-        score.innerHTML = humanScoreNumber
-        result.innerHTML = 'Você ganhou!'
-    } else {
-        machineScoreNumber ++
-        MachineScore.innerHTML = machineScoreNumber
-        result.innerHTML = 'Você perdeu!'
-    }
+if (human === machine){
+    result.innerHTML = 'Deu Empate!'
+} else if (
+(human === 'paper'&& machine ==='rock')||
+(human ==='rock'&& machine ==='scissors')||
+(human === 'scissors'&& machine === 'paper')
+){
+    humanScoreNumber++
+    score.innerHTML = humanScoreNumber
+result.innerHTML = 'VocÊ ganhou!'
+}else {
+    machineScoreNumber++
+    MachineScore.innerHTML = machineScoreNumber
+    result.innerHTML = 'Você perdeu'
+}
 }
